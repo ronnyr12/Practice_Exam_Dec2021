664e884e71831738e92aa03cf3ab6ad4eeabd81a
Ronny <ronny761@hityash.org>
2021-12-21 09:23:59 +0200

    q3 solution, part1

--- src/com/company/Q1.java
+++ src/com/company/Q1.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q1 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
---
+++ src/com/company/Q3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double grade=0, avg=0, avg_min=0;
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 9; j++) {
+
+            }
+        }
+    }
+}
+
